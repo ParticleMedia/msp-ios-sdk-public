ea4108c01baa74a934ed5014b01f2a719672088e
huanzhiNB <huanzhi.zhang@newsbreak.com>
2024-07-09 10:38:25 -0700

    add log files

---
+++ NovaCore/NovaCore/Metrics/MetricService.swift
@@ -0,0 +1,14 @@
+//import HpEngine
+
+//import NBAmpli
+//import NBExperimentManager
+//import NBFirebaseHelper
+//import NBMetricReportable
+//import NBRegionConfigManager
+//import NBApmToolManager
+import Foundation
+
+@objc public class MetricService: NSObject {
+
+}
+
---
+++ NovaCore/NovaCore/Metrics/RealtimeMetricLogger.swift
@@ -0,0 +1,3 @@
+class RealtimeMetricLogger {
+   
+}
