9742bbb9c41623c1c3c31b8a66725d5bdf286d28
huanzhiNB <huanzhi.zhang@newsbreak.com>
2024-11-12 15:30:05 -0800

    preload only when creativw is image

--- NovaAdapter/NovaAdapter/NovaAdapter.swift
+++ NovaAdapter/NovaAdapter/NovaAdapter.swift
@@ -216,10 +216,12 @@ public class NovaAdapter: AdNetworkAdapter {
                 
                     if let adListener = self.adListener,
                        let adRequest = self.adRequest {
-                        appOpenAd?.preloadAdImage() {_ in
-                            DispatchQueue.main.async {
-                                handleAdLoaded(ad: novaInterstitialAd, listener: adListener, adRequest: adRequest)
-                                self.adMetricReporter?.logAdResult(placementId: adRequest.placementId, ad: novaInterstitialAd, fill: true, isFromCache: false)
+                        if appOpenAd?.creativeType == .nativeImage {
+                            appOpenAd?.preloadAdImage() {_ in
+                                DispatchQueue.main.async {
+                                    handleAdLoaded(ad: novaInterstitialAd, listener: adListener, adRequest: adRequest)
+                                    self.adMetricReporter?.logAdResult(placementId: adRequest.placementId, ad: novaInterstitialAd, fill: true, isFromCache: false)
+                                }
                             }
                         }
                     }
