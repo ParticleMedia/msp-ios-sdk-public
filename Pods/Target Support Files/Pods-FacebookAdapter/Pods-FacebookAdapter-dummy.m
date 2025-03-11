2e6f7d856b58c810d0e83431b4662d7b2bf0a094
huanzhiNB <huanzhi.zhang@newsbreak.com>
2025-03-11 13:19:30 -0700

    Remove Pods from tracking

--- Pods/Target Support Files/Pods-FacebookAdapter/Pods-FacebookAdapter-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FacebookAdapter : NSObject
-@end
-@implementation PodsDummy_Pods_FacebookAdapter
-@end
--- Pods/Target Support Files/PubmaticAdapter/PubmaticAdapter-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_PubmaticAdapter : NSObject
-@end
-@implementation PodsDummy_PubmaticAdapter
-@end
