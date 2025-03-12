aa193ac99857c131c296231caeeb32f1391145ed
huanzhiNB <huanzhi.zhang@newsbreak.com>
2025-03-12 14:49:42 -0700

    remove comments

--- GoogleAdapter/GoogleAdapter/GoogleQueryInfoFetcherHelper.swift
+++ GoogleAdapter/GoogleAdapter/GoogleQueryInfoFetcherHelper.swift
@@ -20,7 +20,6 @@ public class GoogleQueryInfoFetcherHelper: GoogleQueryInfoFetcher {
         // Specify the "query_info_type" as "requester_type_8" to
         // denote that the usage of QueryInfo is for Ad Manager S2S.
         let extras = getExtras(adRequest: adRequest)
-        //extras.additionalParameters = ["query_info_type" : "requester_type_8"]
         if let contentUrls = adRequest.customParams[MSPConstants.GOOGLE_AD_MULTI_CONTENT_URLS] as? [String] {
             request.neighboringContentURLStrings = contentUrls
         }
@@ -36,7 +35,7 @@ public class GoogleQueryInfoFetcherHelper: GoogleQueryInfoFetcher {
         default:
             googleAdFormat = GADAdFormat.native
         }
-        //= adRequest.adFormat == .banner ? GADAdFormat.banner : GADAdFormat.native
+        
         GADQueryInfo.createQueryInfo(with: request, adFormat: googleAdFormat) { [weak self] queryInfo, error in
             guard let self = self else {
                 completeListener.onComplete(queryInfo: "")
