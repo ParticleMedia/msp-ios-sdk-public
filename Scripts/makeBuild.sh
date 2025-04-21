45af72cb23370708b1198ca9935c0f117f1aac45
huanzhiNB <huanzhi.zhang@newsbreak.com>
2025-04-21 15:37:14 -0700

    improve build flow

---
+++ Scripts/makeBuild.sh
@@ -0,0 +1,6 @@
+
+# build MSPiOSCore.xcframework and add it to dependency
+Scripts/buildiOSCoreXCFramework.sh
+
+# build NovaCore.xcframework and add it to dependency
+Scripts/buildNovaXCFramework.sh
