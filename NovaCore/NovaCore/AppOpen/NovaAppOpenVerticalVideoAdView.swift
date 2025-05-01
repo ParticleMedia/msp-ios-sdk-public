18d2c625ab0a7df0be69da1c6a62c8a9d41a6031
huanzhiNB <huanzhi.zhang@newsbreak.com>
2025-05-01 13:53:54 -0700

    remove comments

--- NovaCore/NovaCore/AppOpen/NovaAppOpenVerticalVideoAdView.swift
+++ NovaCore/NovaCore/AppOpen/NovaAppOpenVerticalVideoAdView.swift
@@ -261,7 +261,6 @@ private extension NovaAppOpenVerticalVideoAdView {
                 ctaButton.heightAnchor.constraint(equalToConstant: LayoutMetrics.bottomButtonHeight),
                 
                 // closeButton constraints
-                //closeButton.leadingAnchor.constraint(equalTo: nativeAdView.leadingAnchor, constant: LayoutMetrics.horizontalMargin),
                 closeButton.leadingAnchor.constraint(equalTo: nativeAdView.centerXAnchor, constant: LayoutMetrics.horizontalMargin),
                 closeButton.trailingAnchor.constraint(equalTo: ctaButton.leadingAnchor, constant: -LayoutMetrics.horizontalMargin),
                 closeButton.bottomAnchor.constraint(equalTo: nativeAdView.bottomAnchor, constant: -totalButtonBottomMargin),
