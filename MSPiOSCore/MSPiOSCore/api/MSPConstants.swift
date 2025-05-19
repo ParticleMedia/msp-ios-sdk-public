1b417ba70d5317790e1ce163d9d360cab5c5f561
huanzhiNB <huanzhi.zhang@newsbreak.com>
2025-05-19 16:15:47 -0700

    add burl and nurl

--- MSPiOSCore/MSPiOSCore/api/MSPConstants.swift
+++ MSPiOSCore/MSPiOSCore/api/MSPConstants.swift
@@ -8,4 +8,7 @@
 public struct MSPConstants {
     public static let GOOGLE_AD_MULTI_CONTENT_URLS = "google_ads_multi_content_urls"
     public static let USER_ID = "user_id"
+    
+    public static let AD_INFO_OPENRTB_NURL = "nurl"
+    public static let AD_INFO_OPENRTB_BURL = "burl"
 }
