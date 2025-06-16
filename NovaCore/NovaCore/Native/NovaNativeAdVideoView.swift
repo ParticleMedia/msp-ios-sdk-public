0a8fb014bde6e614d743cff422b768431c7031c2
huanzhiNB <huanzhi.zhang@newsbreak.com>
2025-06-16 11:08:35 -0700

    check if user has paused ad

--- NovaCore/NovaCore/Native/NovaNativeAdVideoView.swift
+++ NovaCore/NovaCore/Native/NovaNativeAdVideoView.swift
@@ -151,7 +151,7 @@ public final class NovaNativeAdVideoView: UIView {
     private var lastPauseTime: Double? = nil
 
     private var isOnScreen: Bool = false
-    private var userPausedAd: Bool = false // True: user tapped pause button on the ad, thus should not autolay the video even if it's on screen
+    public var userPausedAd: Bool = false // True: user tapped pause button on the ad, thus should not autolay the video even if it's on screen
 
     private var videoTapRecognizer: UITapGestureRecognizer?
 
