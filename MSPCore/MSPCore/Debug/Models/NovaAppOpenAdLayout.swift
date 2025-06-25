f352b53bd61291ebfe77e1f468955d268a08fc9c
Patrick-Newsbreak <pengyu.gou@newsbreak.com>
2025-06-25 18:01:57 +0800

    feat: Added DebugOptionable & TestParamPresentable

---
+++ MSPCore/MSPCore/Debug/Models/NovaAppOpenAdLayout.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+// copied from NovaCore
+enum NovaAppOpenAdLayout: String, Codable {
+    case horizontal = "horizontal"
+    case vertical = "vertical"
+    case horizontalCancelTopRight = "horizontal_cancel_top_right"
+    case verticalCancelTopRight = "vertical_cancel_top_right"
+    case endCard = "end_card_2_part"
+}
