756e6036e3d120eed254d1bc5ab867a0589bc9c6
huanzhiNB <huanzhi.zhang@newsbreak.com>
2025-07-17 11:07:27 -0700

    resolve comments

--- MSPCore/MSPCore/MSPBidder.swift
+++ MSPCore/MSPCore/MSPBidder.swift
@@ -24,7 +24,7 @@ public class MSPBidder: MSPiOSCore.Bidder {
         self.adRequest = adRequest
         self.bidLoader = prebidBidLoader
         adRequest.customParams["adn_sdk_versions"] = getSDKVersions()
-        for (key,value) in MSPDevice.shared.getContextDictionary() {
+        for (key,value) in MSPDevice.shared.getDeviceSignalsDictionary() {
             adRequest.customParams[key] = value
         }
         prebidBidLoader.loadBid(placementId: bidderPlacementId, adParams: adRequest.customParams, bidListener: self, adRequest: adRequest)
