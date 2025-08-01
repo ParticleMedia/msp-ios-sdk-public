e1fedf4aee072dfde3f55262a824ba424ef8124a
huanzhiNB <huanzhi.zhang@newsbreak.com>
2025-08-01 16:11:19 -0700

    Revert "move click event timing"
    This reverts commit b3838b4f7d7ee84a42b7b8897521e0cf57904726.

--- NovaCore/NovaCore/Native/NovaNativeAdView.swift
+++ NovaCore/NovaCore/Native/NovaNativeAdView.swift
@@ -157,6 +157,11 @@ private extension NovaNativeAdView {
             return
         }
 
+        guard let ctrUrl = nativeAd.ctrUrl else {
+            assertionFailure("Native ad click url cannot be nil")
+            return
+        }
+
         let clickArea = sender.view?.adClickArea
         NovaAdMetricReporter.logAdClick(
             thirdPartyClickTrackingUrls: nativeAd.thirdPartyClickTrackingUrls,
@@ -169,11 +174,6 @@ private extension NovaNativeAdView {
             nativeAd,
             clickAreaName: NovaAdMetricReporter.convertNovaClickAreaNameToMetric(clickArea: clickArea?.rawValue) ?? ""
         )
-        
-        guard let ctrUrl = nativeAd.ctrUrl else {
-            assertionFailure("Native ad click url cannot be nil")
-            return
-        }
 
         let actionKey: String
 
