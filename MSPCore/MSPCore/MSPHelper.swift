10dd1b802c6051879d3e8490eff62183dff59c2c
huanzhiNB <huanzhi.zhang@newsbreak.com>
2025-08-21 10:52:48 -0700

    sample event

--- MSPCore/MSPCore/MSPHelper.swift
+++ MSPCore/MSPCore/MSPHelper.swift
@@ -33,6 +33,10 @@ public class MSP {
     public var ppid: String?
     public var email: String?
     public var prebidAPIKey: String?
+    
+    public var isLogSampled = false
+    public var logWhiteList: [String]?
+    
     public func initMSP(initParams: InitializationParameters, sdkInitListener: MSPInitListener?, adNetworkManagers: [AdNetworkManager]) {
         // This is a temporary solution to replace MSPManager class in kotlin to solve the Kotlin singleton issue
         let initStartTime = Date().timeIntervalSince1970
@@ -253,6 +257,22 @@ public class MSP {
         }
         
         return topViewController
+    }
+    
+    public func updateLogSample(sampleRate: Double) {
+        guard sampleRate.isFinite else {
+            self.isLogSampled = false
+            return
+        }
+        if sampleRate <= 0 {
+            self.isLogSampled = false
+            return
+        }
+        if sampleRate >= 1 {
+            self.isLogSampled = true
+            return
+        }
+        return self.isLogSampled = Double.random(in: 0..<1) < sampleRate
     }
 }
 
