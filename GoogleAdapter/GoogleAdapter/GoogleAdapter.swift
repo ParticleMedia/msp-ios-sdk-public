2d293c62f3275a97e850e4ab2a3165dec30fdd6e
Patrick <pengyu.gou@newsbreak.com>
2025-09-12 22:57:19 +0800

    Release GoogleAdapter version 0.0.2-migration-test-concurrent-fixed
    - Updated podspec version to 0.0.2-migration-test-concurrent-fixed
    - Updated Swift version strings
    - Updated repository URL for CocoaPods publishing
    - Created and pushed git tag 0.0.2-migration-test-concurrent-fixed

--- GoogleAdapter/GoogleAdapter/GoogleAdapter.swift
+++ GoogleAdapter/GoogleAdapter/GoogleAdapter.swift
@@ -6,7 +6,7 @@ import PrebidMobile
 
 @objc public class GoogleAdapter : NSObject, AdNetworkAdapter {
     public func getSDKVersion() -> String {
-        return "0.0.2-migration-test-concurrent"
+        return "0.0.2-migration-test-concurrent-fixed"
     }
     
     public func setAdMetricReporter(adMetricReporter: any MSPiOSCore.AdMetricReporter) {
