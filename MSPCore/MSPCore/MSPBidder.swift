f28454d689e6d66f0f653c82f63c04346a84b51f
huanzhiNB <83240596+huanzhiNB@users.noreply.github.com>
2025-09-16 15:54:51 -0700

    use version in plist (#203)
    * use plist
    
    * remove nova version fall back

--- MSPCore/MSPCore/MSPBidder.swift
+++ MSPCore/MSPCore/MSPBidder.swift
@@ -34,12 +34,25 @@ public class MSPBidder: MSPiOSCore.Bidder {
         let versions: [String: String] = [
             "google": MSP.shared.adNetworkAdapterProvider.getAdNetworkAdapter(adNetwork: .google)?.getSDKVersion() ?? "",
             "facebook": MSP.shared.adNetworkAdapterProvider.getAdNetworkAdapter(adNetwork: .facebook)?.getSDKVersion() ?? "",
-            "nova": MSP.shared.adNetworkAdapterProvider.getAdNetworkAdapter(adNetwork: .nova)?.getSDKVersion() ?? "" // Your method returning a version string
+            "nova": MSP.shared.adNetworkAdapterProvider.getAdNetworkAdapter(adNetwork: .nova)?.getSDKVersion() ?? "",
+            "msp": getMSPVersion()
         ]
 
         if let jsonData = try? JSONSerialization.data(withJSONObject: versions, options: []),
            let jsonString = String(data: jsonData, encoding: .utf8) {
             return jsonString
+        }
+        return ""
+    }
+    
+    private func getMSPVersion() -> String {
+        let bundle = Bundle(for: MSPBidder.self)
+        if let url = bundle.url(forResource: "MSPCoreResources", withExtension: "bundle"),
+           let resourceBundle = Bundle(url: url),
+           let plistURL = resourceBundle.url(forResource: "Info", withExtension: "plist"),
+           let dict = NSDictionary(contentsOf: plistURL),
+           let version = dict["CFBundleShortVersionString"] as? String {
+            return version
         }
         return ""
     }
