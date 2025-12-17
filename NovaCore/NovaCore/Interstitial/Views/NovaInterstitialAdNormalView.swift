efcc21acf8f07d9164359a728fc3d2c605c366fc
huanzhiNB <83240596+huanzhiNB@users.noreply.github.com>
2025-12-16 16:40:58 -0800

    revert interstitial auto dismiss change (#334)
    * Revert "try to fix build (#333)"
    
    This reverts commit c8030ddb0e39c97af14f6fa1d20c3d6297f8d44f.
    
    * Revert "add file (#332)"
    
    This reverts commit dfe15f453d114594dbd47e282a18369d0956c5ca.
    
    * Revert "interstitial auto dismiss on ctr (#329)"
    
    This reverts commit cb9ea207bf5ac681ef1e5fea091ca9dbde4f667f.

--- NovaCore/NovaCore/Interstitial/Views/NovaInterstitialAdNormalView.swift
+++ NovaCore/NovaCore/Interstitial/Views/NovaInterstitialAdNormalView.swift
@@ -87,20 +87,10 @@ class NovaInterstitialAdNormalView: UIView, NovaInterstitialAdViewProtocol {
     }
 
     func didTapAd(customUrl: URL?) {
-        if  NovaConfig.shared.isInParticleApp {
-            viewController?.dismiss(animated: false) {
-                (self.viewController as? NovaInterstitialAdViewController)?.adDidDismiss()
-                self.actionHelper = self.actionHelper
-                    .logNovaClickEvent(with: CACurrentMediaTime() - CACurrentMediaTime(), in: .cta)
-                    .handleAdTap(in: nil)
-                self.context.interstitialAd.delegate?.interstitialAdDidLogClick(self.context.interstitialAd)
-            }
-        } else {
-            actionHelper = actionHelper
-                .logNovaClickEvent(with: CACurrentMediaTime() - CACurrentMediaTime(), in: .cta)
-                .handleAdTap(in: nil)
-            context.interstitialAd.delegate?.interstitialAdDidLogClick(context.interstitialAd)
-        }
+        actionHelper = actionHelper
+            .logNovaClickEvent(with: CACurrentMediaTime() - CACurrentMediaTime(), in: .cta)
+            .handleAdTap(in: nil)
+        context.interstitialAd.delegate?.interstitialAdDidLogClick(context.interstitialAd)
     }
 
 
@@ -115,22 +105,11 @@ class NovaInterstitialAdNormalView: UIView, NovaInterstitialAdViewProtocol {
     }
 
     @objc func didTapAd(sender: UITapGestureRecognizer) {
-        if  NovaConfig.shared.isInParticleApp {
-            viewController?.dismiss(animated: false) {
-                (self.viewController as? NovaInterstitialAdViewController)?.adDidDismiss()
-                let clickArea = sender.view?.adClickArea ?? .cta
-                let nbClickArea = ClickableAdArea(rawValue: clickArea.rawValue) ?? .cta
-                self.actionHelper = self.actionHelper
-                    .logNovaClickEvent(with: CACurrentMediaTime() - self.startTime, in: nbClickArea)
-                    .handleAdTap(in: sender.view)
-            }
-        } else {
-            let clickArea = sender.view?.adClickArea ?? .cta
-            let nbClickArea = ClickableAdArea(rawValue: clickArea.rawValue) ?? .cta
-            actionHelper = self.actionHelper
-                .logNovaClickEvent(with: CACurrentMediaTime() - startTime, in: nbClickArea)
-                .handleAdTap(in: sender.view)
-        }
+        let clickArea = sender.view?.adClickArea ?? .cta
+        let nbClickArea = ClickableAdArea(rawValue: clickArea.rawValue) ?? .cta
+        actionHelper = actionHelper
+            .logNovaClickEvent(with: CACurrentMediaTime() - startTime, in: nbClickArea)
+            .handleAdTap(in: sender.view)
     }
 
     internal let context: NovaInterstitialAdContext
