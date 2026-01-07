b7fe4b5d212698c214a476dc804f1274fae00846
huanzhiNB <83240596+huanzhiNB@users.noreply.github.com>
2026-01-07 10:39:50 -0800

    support report handling (#350)

--- NovaAdapter/NovaAdapter/Interstitial/NovaInterstitialAd.swift
+++ NovaAdapter/NovaAdapter/Interstitial/NovaInterstitialAd.swift
@@ -33,6 +33,9 @@ public class NovaInterstitialAd: MSPiOSCore.InterstitialAd {
         rootViewController: UIViewController?,
         interstitialAdReportHandling: (any InterstitialAdReportHandling)?
     ) {
+        if interstitialAdReportHandling != nil {
+            interstitialAdItem?.supportReportHandling = true
+        }
         if let rootViewController {
             let reportAdapter = ReportHandlerAdapter(outer: interstitialAdReportHandling, ad: self)
             interstitialAdItem?
