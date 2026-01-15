e9c5eea56a8ed16121120366e6f10f852554978e
Patrick <pengyu.gou@newsbreak.com>
2026-01-15 18:41:34 +0800

    chore(release): update MSPAmazonAdapter SDK version to 1.0.0-rc.26
    - Update getSDKVersion() return value to 1.0.0-rc.26
    - Committed immediately after successful publish to CocoaPods
    - Part of release 1.0.0-rc.26 preparation

--- Sources/Adapters/AmazonAdapter/AmazonAdapter/AmazonAdapter.swift
+++ Sources/Adapters/AmazonAdapter/AmazonAdapter/AmazonAdapter.swift
@@ -87,7 +87,7 @@ import DTBiOSSDK
     }
     
     public func getSDKVersion() -> String {
-        return ""
+        return "1.0.0-rc.26"
     }
     
     public func sendClickAdEvent(ad: MSPAd) {
