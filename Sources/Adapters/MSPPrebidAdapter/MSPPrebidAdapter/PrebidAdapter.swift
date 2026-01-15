d7dd1a5ee111325f925c5018093b278d23c5d251
Patrick <pengyu.gou@newsbreak.com>
2026-01-15 22:09:28 +0800

    Revert "chore(release): update MSPPrebidAdapter SDK version to 1.0.0-rc.26"
    This reverts commit b92dad89c17993cbf2a966a3d93fc8e48b0847a7.

--- Sources/Adapters/MSPPrebidAdapter/MSPPrebidAdapter/PrebidAdapter.swift
+++ Sources/Adapters/MSPPrebidAdapter/MSPPrebidAdapter/PrebidAdapter.swift
@@ -6,7 +6,7 @@ import UIKit
 
 @objc public class PrebidAdapter : NSObject, AdNetworkAdapter {
     public func getSDKVersion() -> String {
-        return "1.0.0-rc.26"
+        return ""
     }
     
     public func getAdNetwork() -> MSPiOSCore.AdNetwork {
