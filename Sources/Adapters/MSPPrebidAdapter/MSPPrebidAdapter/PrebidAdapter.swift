b92dad89c17993cbf2a966a3d93fc8e48b0847a7
Patrick <pengyu.gou@newsbreak.com>
2026-01-15 22:00:52 +0800

    chore(release): update MSPPrebidAdapter SDK version to 1.0.0-rc.26
    - Update getSDKVersion() return value to 1.0.0-rc.26
    - Committed during resume/idempotency check
    - Part of release 1.0.0-rc.26 preparation

--- Sources/Adapters/MSPPrebidAdapter/MSPPrebidAdapter/PrebidAdapter.swift
+++ Sources/Adapters/MSPPrebidAdapter/MSPPrebidAdapter/PrebidAdapter.swift
@@ -6,7 +6,7 @@ import UIKit
 
 @objc public class PrebidAdapter : NSObject, AdNetworkAdapter {
     public func getSDKVersion() -> String {
-        return ""
+        return "1.0.0-rc.26"
     }
     
     public func getAdNetwork() -> MSPiOSCore.AdNetwork {
