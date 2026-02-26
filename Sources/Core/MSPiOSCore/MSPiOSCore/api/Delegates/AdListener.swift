b749a28c4d397c6f6a13b1bd48077c1ab2df63db
Mingming Luo <mingming.luo@newsbreak.com>
2026-02-26 13:26:05 +0800

    Fix AdListener compatibility problem (#485)

--- Sources/Core/MSPiOSCore/MSPiOSCore/api/Delegates/AdListener.swift
+++ Sources/Core/MSPiOSCore/MSPiOSCore/api/Delegates/AdListener.swift
@@ -11,12 +11,16 @@ import UIKit
 public protocol AdListener: AnyObject {
     //Deprecated after SDK Bidding:
     //func onAdLoaded(ad: MSPAd)
+    @available(*, deprecated, message: "Use onError(msg:loadInfo:) instead")
+    func onError(msg: String)
     func onError(msg: String, loadInfo: [String: Any])
 
     func onAdImpression(ad: MSPAd)
 
     func onAdClick(ad: MSPAd)
 
+    @available(*, deprecated, message: "Use onAdLoaded(placementId:loadInfo:) instead")
+    func onAdLoaded(placementId: String)
     func onAdLoaded(placementId: String, loadInfo: [String: Any])
 
     func onAdDismissed(ad: InterstitialAd)
