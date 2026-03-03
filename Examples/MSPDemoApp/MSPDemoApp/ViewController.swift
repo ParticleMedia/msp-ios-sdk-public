2ee73ef7b471171fa08a8d0cd2d4a72b8b4ec643
huanzhiNB <83240596+huanzhiNB@users.noreply.github.com>
2026-03-02 16:22:34 -0800

    improve demo app (#490)

--- Examples/MSPDemoApp/MSPDemoApp/ViewController.swift
+++ Examples/MSPDemoApp/MSPDemoApp/ViewController.swift
@@ -1,6 +1,7 @@
 import AppTrackingTransparency
 import MSPCore
 import MSPiOSCore
+import MSPSnapKit
 import UIKit
 
 class ViewController: UIViewController {
@@ -9,194 +10,184 @@ class ViewController: UIViewController {
     public var nativeAdView: NativeAdView?
     public var isCtaShown = false
 
+    private let buttonSpacing: CGFloat = 6
+    private let horizontalPadding: CGFloat = 24
+    private let topPadding: CGFloat = 24
+    private let bottomPadding: CGFloat = 24
+
+    private weak var demoScrollView: UIScrollView?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
 
-        let button1 = UIButton(type: .system)
-        button1.setTitle("Prebid Banner View", for: .normal)
-        button1.addAction(
-            UIAction { [weak self] _ in
-                self?.openDemoAdPage(adType: .prebidBanner)
-            }, for: .touchUpInside)
-        button1.frame = CGRect(x: 100, y: 100, width: 200, height: 50)
-        view.addSubview(button1)
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.showsVerticalScrollIndicator = true
+        scrollView.indicatorStyle = .black
+        scrollView.alwaysBounceVertical = true
+        view.addSubview(scrollView)
+        demoScrollView = scrollView
 
-        let button2 = UIButton(type: .system)
-        button2.setTitle("Google Banner View", for: .normal)
-        button2.frame = CGRect(x: 100, y: 150, width: 200, height: 50)
-        view.addSubview(button2)
+        let contentStack = UIStackView()
+        contentStack.axis = .vertical
+        contentStack.spacing = buttonSpacing
+        contentStack.alignment = .fill
+        contentStack.distribution = .equalSpacing
+        contentStack.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.addSubview(contentStack)
+
+        let button1 = makeButton(title: "Prebid Banner View") { [weak self] in
+            self?.openDemoAdPage(adType: .prebidBanner)
+        }
+
+        let button2 = makeButton(title: "Google Banner View")
         let googleBannerMenuItems = [
-            UIAction(title: "s2s", handler: { _ in self.openDemoAdPage(adType: .googleBanner) }),
-            UIAction(title: "c2s", handler: { _ in self.openDemoAdPage(adType: .googleBannerC2S) }),
+            UIAction(title: "s2s", handler: { [weak self] _ in self?.openDemoAdPage(adType: .googleBanner) }),
+            UIAction(title: "c2s", handler: { [weak self] _ in self?.openDemoAdPage(adType: .googleBannerC2S) }),
         ]
-
         button2.menu = UIMenu(title: "Choose an option", children: googleBannerMenuItems)
         button2.showsMenuAsPrimaryAction = true
-        let button3 = UIButton(type: .system)
-        button3.setTitle("Google Native View", for: .normal)
-        button3.frame = CGRect(x: 100, y: 200, width: 200, height: 50)
-        view.addSubview(button3)
-        let googleNativeMenuItems = [
-            UIAction(title: "s2s", handler: { _ in self.openDemoAdPage(adType: .googleNative) }),
-            UIAction(title: "c2s", handler: { _ in self.openDemoAdPage(adType: .googleNativeC2S) }),
-        ]
 
+        let button3 = makeButton(title: "Google Native View")
+        let googleNativeMenuItems = [
+            UIAction(title: "s2s", handler: { [weak self] _ in self?.openDemoAdPage(adType: .googleNative) }),
+            UIAction(title: "c2s", handler: { [weak self] _ in self?.openDemoAdPage(adType: .googleNativeC2S) }),
+        ]
         button3.menu = UIMenu(title: "Choose an option", children: googleNativeMenuItems)
         button3.showsMenuAsPrimaryAction = true
-        let button4 = UIButton(type: .system)
-        button4.setTitle("Nova Native View", for: .normal)
-        button4.addAction(
-            UIAction { [weak self] _ in
-                self?.openDemoAdPage(adType: .novaNative)
-            }, for: .touchUpInside)
-        button4.frame = CGRect(x: 100, y: 250, width: 200, height: 50)
-        view.addSubview(button4)
-        let button5 = UIButton(type: .system)
-        button5.setTitle("Google Interstitial View", for: .normal)
-        button5.frame = CGRect(x: 100, y: 300, width: 200, height: 50)
-        view.addSubview(button5)
-        let googleInterstitialMenuItems = [
-            UIAction(title: "s2s", handler: { _ in self.openDemoAdPage(adType: .googleInterstitial) }),
-            UIAction(title: "c2s", handler: { _ in self.openDemoAdPage(adType: .googleInterstitialC2S) }),
-        ]
 
+        let button4 = makeButton(title: "Nova Native View") { [weak self] in
+            self?.openDemoAdPage(adType: .novaNative)
+        }
+
+        let button5 = makeButton(title: "Google Interstitial View")
+        let googleInterstitialMenuItems = [
+            UIAction(title: "s2s", handler: { [weak self] _ in self?.openDemoAdPage(adType: .googleInterstitial) }),
+            UIAction(title: "c2s", handler: { [weak self] _ in self?.openDemoAdPage(adType: .googleInterstitialC2S) }),
+        ]
         button5.menu = UIMenu(title: "Choose an option", children: googleInterstitialMenuItems)
         button5.showsMenuAsPrimaryAction = true
-        let button6 = UIButton(type: .system)
-        button6.setTitle("Nova Interstitial View", for: .normal)
-        button6.frame = CGRect(x: 100, y: 350, width: 200, height: 50)
-        view.addSubview(button6)
+
+        let button6 = makeButton(title: "Nova Interstitial View")
         let novaInterstitialMenuItems = [
-            UIAction(
-                title: "Horizontal Image",
-                handler: { _ in self.openDemoAdPage(adType: .novaInterstitialHorizontalImage) }),
-            UIAction(
-                title: "Vertical Image", handler: { _ in self.openDemoAdPage(adType: .novaInterstitialVerticalImage) }),
-            UIAction(
-                title: "Horizontal Video",
-                handler: { _ in self.openDemoAdPage(adType: .novaInterstitialHorizontalVideo) }),
-            UIAction(
-                title: "Vertical Video", handler: { _ in self.openDemoAdPage(adType: .novaInterstitialVerticalVideo) }),
-            UIAction(
-                title: "High Engagement", handler: { _ in self.openDemoAdPage(adType: .novaInterstitialHighEngagement) }
-            ),
-            UIAction(
-                title: "End Card 2 Parts", handler: { _ in self.openDemoAdPage(adType: .novaInterstitialEndCard) }),
+            UIAction(title: "Horizontal Image", handler: { [weak self] _ in self?.openDemoAdPage(adType: .novaInterstitialHorizontalImage) }),
+            UIAction(title: "Vertical Image", handler: { [weak self] _ in self?.openDemoAdPage(adType: .novaInterstitialVerticalImage) }),
+            UIAction(title: "Horizontal Video", handler: { [weak self] _ in self?.openDemoAdPage(adType: .novaInterstitialHorizontalVideo) }),
+            UIAction(title: "Vertical Video", handler: { [weak self] _ in self?.openDemoAdPage(adType: .novaInterstitialVerticalVideo) }),
+            UIAction(title: "High Engagement", handler: { [weak self] _ in self?.openDemoAdPage(adType: .novaInterstitialHighEngagement) }),
+            UIAction(title: "End Card 2 Parts", handler: { [weak self] _ in self?.openDemoAdPage(adType: .novaInterstitialEndCard) }),
         ]
         button6.menu = UIMenu(title: "Choose an option", children: novaInterstitialMenuItems)
         button6.showsMenuAsPrimaryAction = true
 
-        let button7 = UIButton(type: .system)
-        button7.setTitle("Facebook Native View", for: .normal)
-        button7.addAction(
-            UIAction { [weak self] _ in
-                self?.openDemoAdPage(adType: .facebookNative)
-            }, for: .touchUpInside)
-        button7.frame = CGRect(x: 100, y: 400, width: 200, height: 50)
-        view.addSubview(button7)
+        let button7 = makeButton(title: "Facebook Native View") { [weak self] in
+            self?.openDemoAdPage(adType: .facebookNative)
+        }
 
-        let button8 = UIButton(type: .system)
-        button8.setTitle("Facebook Interstitial View", for: .normal)
-        button8.addAction(
-            UIAction { [weak self] _ in
-                self?.openDemoAdPage(adType: .facebookInterstitial)
-            }, for: .touchUpInside)
-        button8.frame = CGRect(x: 100, y: 450, width: 200, height: 50)
-        view.addSubview(button8)
+        let button8 = makeButton(title: "Facebook Interstitial View") { [weak self] in
+            self?.openDemoAdPage(adType: .facebookInterstitial)
+        }
 
-        let button9 = UIButton(type: .system)
-        button9.setTitle("C2S Bidders Banner View", for: .normal)
-        button9.frame = CGRect(x: 100, y: 500, width: 200, height: 50)
-        view.addSubview(button9)
+        let button9 = makeButton(title: "C2S Bidders Banner View")
         let bannerMenuItems = [
-            UIAction(title: "Unity", handler: { _ in self.openDemoAdPage(adType: .unityBanner) }),
-            UIAction(title: "Pubmatic", handler: { _ in self.openDemoAdPage(adType: .pubmaticBanner) }),
-            UIAction(title: "Inmobi", handler: { _ in self.openDemoAdPage(adType: .inmobiBanner) }),
-            UIAction(title: "Mobilefuse", handler: { _ in self.openDemoAdPage(adType: .mobilefuseBanner) }),
-            UIAction(title: "Mintegral", handler: { _ in self.openDemoAdPage(adType: .mintegralBanner) }),
+            UIAction(title: "Unity", handler: { [weak self] _ in self?.openDemoAdPage(adType: .unityBanner) }),
+            UIAction(title: "Pubmatic", handler: { [weak self] _ in self?.openDemoAdPage(adType: .pubmaticBanner) }),
+            UIAction(title: "Inmobi", handler: { [weak self] _ in self?.openDemoAdPage(adType: .inmobiBanner) }),
+            UIAction(title: "Mobilefuse", handler: { [weak self] _ in self?.openDemoAdPage(adType: .mobilefuseBanner) }),
+            UIAction(title: "Mintegral", handler: { [weak self] _ in self?.openDemoAdPage(adType: .mintegralBanner) }),
         ]
-
         button9.menu = UIMenu(title: "Choose an option", children: bannerMenuItems)
         button9.showsMenuAsPrimaryAction = true
 
-
-        let button10 = UIButton(type: .system)
-        button10.setTitle("C2S Bidders Interstitial View", for: .normal)
-        button10.frame = CGRect(x: 100, y: 550, width: 200, height: 50)
-        view.addSubview(button10)
+        let button10 = makeButton(title: "C2S Bidders Interstitial View")
         let interstitialMenuItems = [
-            UIAction(title: "Unity", handler: { _ in self.openDemoAdPage(adType: .unityInterstitial) }),
-            UIAction(title: "Pubmatic", handler: { _ in self.openDemoAdPage(adType: .pubmaticInterstitial) }),
-            UIAction(title: "Inmobi", handler: { _ in self.openDemoAdPage(adType: .inmobiInterstitial) }),
-            UIAction(title: "Mobilefuse", handler: { _ in self.openDemoAdPage(adType: .mobilefuseInterstitial) }),
-            UIAction(title: "Mintegral", handler: { _ in self.openDemoAdPage(adType: .mintegralInterstitial) }),
+            UIAction(title: "Unity", handler: { [weak self] _ in self?.openDemoAdPage(adType: .unityInterstitial) }),
+            UIAction(title: "Pubmatic", handler: { [weak self] _ in self?.openDemoAdPage(adType: .pubmaticInterstitial) }),
+            UIAction(title: "Inmobi", handler: { [weak self] _ in self?.openDemoAdPage(adType: .inmobiInterstitial) }),
+            UIAction(title: "Mobilefuse", handler: { [weak self] _ in self?.openDemoAdPage(adType: .mobilefuseInterstitial) }),
+            UIAction(title: "Mintegral", handler: { [weak self] _ in self?.openDemoAdPage(adType: .mintegralInterstitial) }),
         ]
-
         button10.menu = UIMenu(title: "Choose an option", children: interstitialMenuItems)
         button10.showsMenuAsPrimaryAction = true
 
-
-        let button11 = UIButton(type: .system)
-        button11.setTitle("C2S Bidders Native View", for: .normal)
-        button11.frame = CGRect(x: 100, y: 600, width: 200, height: 50)
-        view.addSubview(button11)
+        let button11 = makeButton(title: "C2S Bidders Native View")
         let nativeMenuItems = [
-            UIAction(title: "Unity", handler: { _ in self.openDemoAdPage(adType: .unityNative) }),
-            UIAction(title: "Pubmatic", handler: { _ in self.openDemoAdPage(adType: .pubmaticNative) }),
-            UIAction(title: "Inmobi", handler: { _ in self.openDemoAdPage(adType: .inmobiNative) }),
-            UIAction(title: "Mobilefuse", handler: { _ in self.openDemoAdPage(adType: .mobilefuseNative) }),
-            UIAction(title: "Mintegral", handler: { _ in self.openDemoAdPage(adType: .mintegralNative) }),
+            UIAction(title: "Unity", handler: { [weak self] _ in self?.openDemoAdPage(adType: .unityNative) }),
+            UIAction(title: "Pubmatic", handler: { [weak self] _ in self?.openDemoAdPage(adType: .pubmaticNative) }),
+            UIAction(title: "Inmobi", handler: { [weak self] _ in self?.openDemoAdPage(adType: .inmobiNative) }),
+            UIAction(title: "Mobilefuse", handler: { [weak self] _ in self?.openDemoAdPage(adType: .mobilefuseNative) }),
+            UIAction(title: "Mintegral", handler: { [weak self] _ in self?.openDemoAdPage(adType: .mintegralNative) }),
         ]
-
         button11.menu = UIMenu(title: "Choose an option", children: nativeMenuItems)
         button11.showsMenuAsPrimaryAction = true
 
+        let button12 = makeButton(title: "Client Bidding Banner") { [weak self] in
+            self?.openDemoAdPage(adType: .clientBiddingBanner)
+        }
 
-        let button12 = UIButton(type: .system)
-        button12.setTitle("Client Bidding Banner", for: .normal)
-        button12.addAction(
-            UIAction { [weak self] _ in
-                self?.openDemoAdPage(adType: .clientBiddingBanner)
-            }, for: .touchUpInside)
-        button12.frame = CGRect(x: 100, y: 650, width: 200, height: 50)
-        view.addSubview(button12)
-
-        let button13 = UIButton(type: .system)
-        button13.setTitle("Prebid Interstitial", for: .normal)
-        button13.addAction(
-            UIAction { [weak self] _ in
-                self?.openDemoAdPage(adType: .prebidInterstitial)
-            }, for: .touchUpInside)
-        button13.frame = CGRect(x: 100, y: 700, width: 200, height: 50)
-        view.addSubview(button13)
+        let button13 = makeButton(title: "Prebid Interstitial") { [weak self] in
+            self?.openDemoAdPage(adType: .prebidInterstitial)
+        }
 
         let adListButton = UIButton(type: .system)
         adListButton.setTitle("Ad List (Reuse Test)", for: .normal)
         adListButton.backgroundColor = .systemPurple
         adListButton.setTitleColor(.white, for: .normal)
         adListButton.layer.cornerRadius = 8
-        adListButton.addAction(
-            UIAction { [weak self] _ in
-                let vc = DemoAdListViewController()
-                self?.navigationController?.pushViewController(vc, animated: true)
-            }, for: .touchUpInside)
-        adListButton.frame = CGRect(x: 100, y: 750, width: 200, height: 50)
-        view.addSubview(adListButton)
+        adListButton.addAction(UIAction { [weak self] _ in
+            let vc = DemoAdListViewController()
+            self?.navigationController?.pushViewController(vc, animated: true)
+        }, for: .touchUpInside)
+        adListButton.snp.makeConstraints { make in make.height.equalTo(50) }
 
         let debugButton = UIButton(type: .system)
         debugButton.setTitle("Debug Ad Load", for: .normal)
         debugButton.backgroundColor = .systemOrange
         debugButton.setTitleColor(.white, for: .normal)
         debugButton.layer.cornerRadius = 8
-        debugButton.addAction(
-            UIAction { _ in
-                MSP.shared.showMediationDebugger()
-            }, for: .touchUpInside)
-        debugButton.frame = CGRect(x: 100, y: 810, width: 200, height: 50)
-        view.addSubview(debugButton)
+        debugButton.addAction(UIAction { _ in MSP.shared.showMediationDebugger() }, for: .touchUpInside)
+        debugButton.snp.makeConstraints { make in make.height.equalTo(50) }
+
+        let standardButtons: [UIButton] = [
+            button1, button2, button3, button4, button5, button6, button7, button8,
+            button9, button10, button11, button12, button13,
+        ]
+        standardButtons.forEach { contentStack.addArrangedSubview($0) }
+        contentStack.addArrangedSubview(adListButton)
+        contentStack.addArrangedSubview(debugButton)
+
+        scrollView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+
+        contentStack.snp.makeConstraints { make in
+            make.top.equalTo(scrollView.contentLayoutGuide.snp.top).offset(topPadding)
+            make.leading.equalTo(scrollView.contentLayoutGuide.snp.leading).offset(horizontalPadding)
+            make.trailing.equalTo(scrollView.contentLayoutGuide.snp.trailing).offset(-horizontalPadding)
+            make.bottom.equalTo(scrollView.contentLayoutGuide.snp.bottom).offset(-bottomPadding)
+            make.width.equalTo(scrollView.frameLayoutGuide.snp.width).offset(-2 * horizontalPadding)
+        }
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        demoScrollView?.flashScrollIndicators()
+    }
+
+    private func makeButton(title: String, action: (() -> Void)? = nil) -> UIButton {
+        let button = UIButton(type: .system)
+        button.setTitle(title, for: .normal)
+        button.titleLabel?.numberOfLines = 2
+        button.titleLabel?.textAlignment = .center
+        if let action = action {
+            button.addAction(UIAction { _ in action() }, for: .touchUpInside)
+        }
+        button.snp.makeConstraints { make in
+            make.height.equalTo(50)
+        }
+        return button
+    }
 
     func openDemoAdPage(adType: AdType) {
         let demoAdVC = DemoAdViewController(adType: adType)
