0ec75b14fa31eb987223828d252f580defc48c78
Mingming Luo <mingming.luo@newsbreak.com>
2026-04-02 14:15:01 +0800

    Fix volume level retrieval blocking background audio (#603)
    * Fix get volume level blocking background app's audio
    
    * Remove operation on audio session

--- Sources/Core/MSPCore/MSPCore/MSPDevice.swift
+++ Sources/Core/MSPCore/MSPCore/MSPDevice.swift
@@ -272,12 +272,6 @@ public class MSPDevice {
 
     internal func getVolumeLevel() -> Int32 {
         let audioSession = AVAudioSession.sharedInstance()
-        do {
-            try audioSession.setActive(true)
-            let volume = audioSession.outputVolume
-            return Int32(volume * 100)
-        } catch {
-            return -1
-        }
+        return Int32(audioSession.outputVolume * 100)
     }
 }
